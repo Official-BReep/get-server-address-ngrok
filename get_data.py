9be5758bfb985f03848dc8e8598f69166f7c26b8
BReep <67698695+Official-BReep@users.noreply.github.com>
2023-02-14 09:16:04 +0100

    Create get_data.py

---
+++ get_data.py
@@ -0,0 +1,13 @@
+from pyngrok import ngrok
+
+ngrok.set_auth_token("AUTH-TOKEN HERE")
+api = ngrok.get_ngrok_process().api_url #get api_url
+print(api)
+response = ngrok.api_request("{}/api/tunnels".format(str(api).replace("4041", "4040") if "4041" in api else str(api)),  #replace to fix error if ngrok is running
+                             params={"tunnel_name": "foo"})
+try:
+    data = response['tunnels'][0] #get data from response
+    host, port = str(data['public_url'][6:]).split(":") #get server ip and server port
+    print(host,":",port)
+except:
+    print(response['tunnels'])
