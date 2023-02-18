9e41a03ba27ede085136e186fafe1385242264ed
BReep <67698695+Official-BReep@users.noreply.github.com>
2023-02-18 13:53:12 +0100

    Update get_data.py
    shorter and better version

--- get_data.py
+++ get_data.py
@@ -1,11 +1,8 @@
-from pyngrok import ngrok
+import ngrok
 
-ngrok.set_auth_token("AUTH-TOKEN HERE")
-api = ngrok.get_ngrok_process().api_url #get api_url
-response = ngrok.api_request("{}/api/tunnels".format(str(api).replace("4041", "4040") if "4041" in api else str(api)),  #replace to fix error if ngrok is running
-                             params={"tunnel_name": "foo"})
-try:
-    data = response['tunnels'][0] #get data from response
-    host, port = str(data['public_url'][6:]).split(":") #get server ip and server port
-except:
-    pass
+# construct the api client
+client = ngrok.Client("API-KEY")
+
+# list all online tunnels
+for t in client.tunnels.list():
+    host, port = t.public_url[6:]
